0cef41bf4b504bad24bd531b081577d13f99c580
aqurds <omarf1320@gmail.com>
2023-05-23 22:06:37 +0600

    Stylelint & ESLint error fix V1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,17 @@ const openMobileMenu = document.querySelector('#mobile-menu-open');
 const closeMobileMenu = document.querySelector('.mobile-menu-close');
 const mobileMenuWrapper = document.querySelector('.top-nav-mobile');
 const mobileMenuState = {
-  open: "block",
-  close: 'none'
-}
+  open: 'block',
+  close: 'none',
+};
 
 function toggleMobileMenu(state) {
-  if (state == mobileMenuState.open) {
+  if (state === mobileMenuState.open) {
     mobileMenuWrapper.style.display = mobileMenuState.open;
   } else {
     mobileMenuWrapper.style.display = mobileMenuState.close;
   }
 }
 
-openMobileMenu.addEventListener('click', () => { toggleMobileMenu(mobileMenuState.open) });
-closeMobileMenu.addEventListener('click', () => { toggleMobileMenu(mobileMenuState.close) })
+openMobileMenu.addEventListener('click', () => { toggleMobileMenu(mobileMenuState.open); });
+closeMobileMenu.addEventListener('click', () => { toggleMobileMenu(mobileMenuState.close); });
